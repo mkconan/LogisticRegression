94c56aec18cb8bb9e7d5321a383df6f83ee242e4
MkConan <koutarouelectro@gmail.com>
2024-06-15 18:12:41 +0900

    update script

--- judge_is_sruvived_in_titanic.py
+++ judge_is_sruvived_in_titanic.py
@@ -8,20 +8,49 @@ from pprint import pprint
 import pandas as pd
 from itertools import combinations
 import os
+import pickle
 
-features = ["PassengerId", "Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
+features = ["PassengerId", "Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked", "Ticket"]
 object_value_name = "Survived"
 
 
-def load_data(path: str):
+def load_data(path: str, is_train: bool = True):
     df = pd.read_csv(path)
+
     # filter
-    df = df[[*features, object_value_name]]
-    # drop if exists na data
-    df = df.dropna()
+    if is_train:
+        df = df[[*features, object_value_name]]
+    else:  # for eval
+        df = df[[*features]]
+
+    # 欠損データに対する処理
+    # df = df.dropna()
+    df["Age"] = df["Age"].fillna(df["Age"].mean())
+    df["Fare"] = df["Fare"].fillna(df["Fare"].mean())
+    df["Embarked"] = df["Embarked"].fillna("S")
+
     # mapping
     df["Sex"] = df["Sex"].map({"male": 0, "female": 1})
     df["Embarked"] = df["Embarked"].map({"C": 0, "Q": 1, "S": 2})
+    return df
+
+
+def pre_process_data(df: pd.DataFrame):
+    # 料金0は除外
+    df = df[df["Fare"] != 0].reset_index(drop=True)
+
+    # ticket
+    ticket_values = df["Ticket"].value_counts()
+    ticket_values = ticket_values[ticket_values > 1]
+    ticket_values = pd.Series(ticket_values.index, name="Ticket")
+    categories = set(ticket_values.tolist())
+    df["Ticket"] = pd.Categorical(df["Ticket"], categories=categories)
+    df = pd.get_dummies(df, columns=["Ticket"])
+
+    # SibSp/Parch
+    df = pd.get_dummies(df, columns=["SibSp"])
+    df = pd.get_dummies(df, columns=["Parch"])
+
     return df
 
 
@@ -47,34 +76,51 @@ def pair_plot(df: pd.DataFrame):
         plt.close()
 
 
-def main():
+def train():
     df = load_data("./train.csv")
+    df = pre_process_data(df)
     # 使えそうな特徴量をさがす
     # pair_plot(df)
 
     # 質的データはダミー変数に変換
-    df = pd.get_dummies(df, drop_first=True)
+    # df = pd.get_dummies(df, drop_first=True)
 
     X = df.drop(object_value_name, axis=1)
     Y = df[object_value_name]
 
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
+    X_train, X_eval, Y_train, Y_eval = train_test_split(X, Y, test_size=0.2, random_state=0)
 
     lr = LogisticRegression(penalty="l1", solver="liblinear")
     result = lr.fit(X_train, Y_train)  # ロジスティック回帰モデルの重みを学習
     pprint(result.get_params())
 
-    Y_pred = lr.predict(X_test)
+    Y_pred = lr.predict(X_eval)
 
-    print("confusion matrix = \n", confusion_matrix(y_true=Y_test, y_pred=Y_pred))
-    print("accuracy = ", accuracy_score(y_true=Y_test, y_pred=Y_pred))
-    print("precision = ", precision_score(y_true=Y_test, y_pred=Y_pred))
-    print("recall = ", recall_score(y_true=Y_test, y_pred=Y_pred))
-    print("f1 score = ", f1_score(y_true=Y_test, y_pred=Y_pred))
+    print("confusion matrix = \n", confusion_matrix(y_true=Y_eval, y_pred=Y_pred))
+    print("accuracy = ", accuracy_score(y_true=Y_eval, y_pred=Y_pred))
+    print("precision = ", precision_score(y_true=Y_eval, y_pred=Y_pred))
+    print("recall = ", recall_score(y_true=Y_eval, y_pred=Y_pred))
+    print("f1 score = ", f1_score(y_true=Y_eval, y_pred=Y_pred))
 
-    Y_score = lr.predict_proba(X_test)[:, 1]  # 検証データがクラス1に属する確率
-    print("auc = ", roc_auc_score(y_true=Y_test, y_score=Y_score))
+    Y_score = lr.predict_proba(X_eval)[:, 1]  # 検証データがクラス1に属する確率
+    print("auc = ", roc_auc_score(y_true=Y_eval, y_score=Y_score))
+
+    with open("model.pickle", mode="wb") as f:
+        pickle.dump(lr, f)
+
+
+def test():
+    X = load_data("./test.csv", is_train=False)
+    print(X["PassengerId"])
+    with open("./model.pickle", mode="rb") as f:
+        model = pickle.load(f)
+        Y = model.predict(X)
+
+    predict_result = pd.Series(Y, name=object_value_name)
+    result_df = pd.concat([X["PassengerId"], predict_result], axis=1)
+    result_df.to_csv("./result_submission.csv", index=False)
 
 
 if __name__ == "__main__":
-    main()
+    train()
+    # test()
